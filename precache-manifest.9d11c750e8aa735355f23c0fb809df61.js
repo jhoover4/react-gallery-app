cb0a64ac69a768ee520eadb0c8fa13ea60f3b1df
jhoover4 <jordan@hoovermld.com>
2019-10-27 17:52:08 -0500

    Updates

---
+++ precache-manifest.9d11c750e8aa735355f23c0fb809df61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230bf59a5b4204cf37b2aed35c830182",
+    "url": "/react-gallery-app/index.html"
+  },
+  {
+    "revision": "b038671a0a6ac8331baa",
+    "url": "/react-gallery-app/static/css/main.914ea0b3.chunk.css"
+  },
+  {
+    "revision": "a332cdcbc7d950d41e1a",
+    "url": "/react-gallery-app/static/js/2.1777d272.chunk.js"
+  },
+  {
+    "revision": "b038671a0a6ac8331baa",
+    "url": "/react-gallery-app/static/js/main.27392f9f.chunk.js"
+  },
+  {
+    "revision": "3030b2d0434baacddf9e",
+    "url": "/react-gallery-app/static/js/runtime-main.c8c05b53.js"
+  }
+]);
