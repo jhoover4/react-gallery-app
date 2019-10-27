27009576d1a4101cf67999db6a69e429fe928c16
jhoover4 <jordan@hoovermld.com>
2019-10-27 11:31:58 -0500

    Create initial components.

---
+++ src/components/Photo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Photo extends Component {
+  render() {
+    return (
+      <li>
+        <img
+          src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg"
+          alt=""
+        />
+      </li>
+    );
+  }
+}
+
+export default Photo;
